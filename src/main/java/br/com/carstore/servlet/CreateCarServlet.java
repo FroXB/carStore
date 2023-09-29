696333c02ac999c3e412b67d26faac23bcde265f
Rafael Barbosa <rafaelbarbosa1199@gmail.com>
2023-09-29 20:44:15 -0300

    add bd

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 
 @WebServlet("/create-car")
 public class CreateCarServlet extends HttpServlet {
@@ -15,7 +18,14 @@ public class CreateCarServlet extends HttpServlet {
 
         String carName = req.getParameter("car-name");
 
-        System.out.println(carName);
+        Car carro = new Car();
+        CarDao carDao = new CarDao();
+
+        carro.setName(carName);
+
+        carDao.createCar(carro);
+
+
 
         req.getRequestDispatcher("index.html").forward(req, resp);
 
